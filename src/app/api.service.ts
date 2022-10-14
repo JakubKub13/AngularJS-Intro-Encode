302e0e2c5988c11fda77bf05ec1f98db608627f4
JK1331 <jakubkubala3@gmail.com>
2022-10-14 16:24:47 +0200

    Connecting FE to BE

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getContractAddress(): Observable<string> {
+    return this.http.get<string>("http://localhost:3000/token-address");
+  }
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ethers, Signer } from 'ethers';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,22 +8,32 @@ import { ethers, Signer } from 'ethers';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  tokenContractAddress: string;
   walletAddress: string;
   wallet: ethers.Wallet | undefined;
   etherBalance: string;
   provider: ethers.providers.BaseProvider;
 
-  constructor() {
+  constructor(private apiService: ApiService) {
     this.walletAddress = "Loading.....";
     this.etherBalance = "Loading.....";
+    this.tokenContractAddress = "";
     this.provider = ethers.getDefaultProvider("goerli");
    }
 
   ngOnInit(): void {
+    this.apiService.getContractAddress().subscribe((response) => {
+      this.tokenContractAddress = response;
+    });
     this.wallet = ethers.Wallet.createRandom();
     this.walletAddress = this.wallet.address;
     this.provider.getBalance(this.walletAddress).then((balanceBN) => {
       this.etherBalance = ethers.utils.formatEther(balanceBN) + " ETH";
     });
+  }
+
+  doAction() {
+    this.wallet = ethers.Wallet.createRandom();
+    this.walletAddress = this.wallet.address;
   }
 }
